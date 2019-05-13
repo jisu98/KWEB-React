08961968b4fb047f712a6f309b03e2716c4af23e
jisu98 <imsrhs60l@korea.ac.kr>
2019-05-13 15:42:29 +0900

    ~ Chapter 7

---
+++ phone-book/src/components/PhoneInfoList.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PhoneInfo from './PhoneInfo';
+
+class PhoneInfoList extends Component {
+  static defaultProps = {
+    data: []
+  }
+
+  render() {
+    const { data } = this.props;
+    const list = data.map(
+      info => (<PhoneInfo key={info.id} info={info}/>)
+    );
+    /*
+    const list = data.map(
+     (info, index) => (<PhoneInfo key={index} info={info}/>)
+    );
+    */
+
+    return (
+      <div>
+        {list}    
+      </div>
+    );
+  }
+}
+
+export default PhoneInfoList;
