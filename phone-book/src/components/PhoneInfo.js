223f8a401a817f1a9becbe48cc0c5dfd61fd8554
jisu98 <imsrhs60l@korea.ac.kr>
2019-05-13 15:55:38 +0900

    ~ Chapter 9

--- phone-book/src/components/PhoneInfo.js
+++ phone-book/src/components/PhoneInfo.js
@@ -42,6 +42,16 @@ class PhoneInfo extends Component {
     });
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    // 수정 상태가 아니고, info 값이 같다면 리렌더링 안함
+    if (!this.state.editing  
+        && !nextState.editing
+        && nextProps.info === this.props.info) {
+      return false;
+    }
+    // 나머지 경우엔 리렌더링함
+    return true;
+  }
 
   componentDidUpdate(prevProps, prevState) {
     // 여기서는, editing 값이 바뀔 때 처리 할 로직이 적혀있습니다.
@@ -68,6 +78,7 @@ class PhoneInfo extends Component {
   }
   
   render() {
+    console.log('render PhoneInfo ' + this.props.info.id);
     const style = {
       border: '1px solid black',
       padding: '8px',
